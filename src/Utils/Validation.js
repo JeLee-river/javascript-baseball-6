7387945baad1b1c60af470250400cde18627e614
jeongeunLee <leeju1009@gmail.com>
2023-12-12 00:23:16 +0900

    feat: 사용자가 입력한 숫자 3개가 중복되는지 검사하는 함수 추가

--- src/Utils/Validation.js
+++ src/Utils/Validation.js
@@ -8,6 +8,18 @@ const Validation = {
       );
     }
   },
+
+  isDifferentNumber: (numbers) => {
+    const numberArray = numbers.split('');
+    const convertedNumberArray = numberArray.map(Number);
+    const numberSet = new Set([...convertedNumberArray]);
+
+    if (numberArray.length !== numberSet.size) {
+      throw new Error(
+        '[ERROR] 유효하지 않은 숫자입니다. 서로 다른 숫자 3개를 입력해주세요.',
+      );
+    }
+  },
 };
 
 export default Validation;
