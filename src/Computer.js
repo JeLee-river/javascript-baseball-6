0465a569f18f43f674c22a05c9873465360adc5c
jeongeunLee <leeju1009@gmail.com>
2023-12-11 22:49:06 +0900

    feat: 게임 결과를 계산하는 기능 추가

--- src/Computer.js
+++ src/Computer.js
@@ -19,6 +19,22 @@ class Computer {
 
     return answerArray;
   }
+
+  #calculateResult(numbers) {
+    const baseballCounter = { strike: 0, ball: 0 };
+    this.#answer.forEach((number, index) => {
+      const foundIndex = numbers.indexOf(number);
+      if (foundIndex === index) {
+        baseballCounter.strike += 1;
+      }
+
+      if (foundIndex !== -1) {
+        baseballCounter.ball += 1;
+      }
+
+      return baseballCounter;
+    });
+  }
 }
 
 export default Computer;
