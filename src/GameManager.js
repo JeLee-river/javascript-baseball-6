3ef13e436787c35aeeb821a5fc2cf8788888697c
jeongeunLee <leeju1009@gmail.com>
2023-12-12 00:00:04 +0900

    refactor: Computer 객체의 게임 결과 계산함수를 GameManager 객체로 이동

--- src/GameManager.js
+++ src/GameManager.js
@@ -1,8 +1,35 @@
 class GameManager {
   #numbers;
 
-  constructor(numbers) {
-    this.#numbers = numbers;
+  #answer;
+
+  constructor(userNumbers, answer) {
+    this.#numbers = userNumbers;
+    this.#answer = answer;
+  }
+
+  #calculateResult(numbers) {
+    const baseballCounter = { strike: 0, ball: 0 };
+    this.#answer.forEach((number, index) => {
+      const foundIndex = numbers.indexOf(number);
+      if (foundIndex === index) {
+        baseballCounter.strike += 1;
+      }
+
+      if (foundIndex !== -1) {
+        baseballCounter.ball += 1;
+      }
+    });
+
+    return baseballCounter;
+  }
+
+  showResult(userNumbers) {
+    const { strike, ball } = this.#calculateResult(userNumbers);
+    return new Map([
+      ['스트라이크', strike],
+      ['볼', ball],
+    ]);
   }
 }
 
