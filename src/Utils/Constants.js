674ca1ab7c04346d744a6a07d2cbb89be7fef5e6
jeongeunLee <leeju1009@gmail.com>
2023-12-12 13:36:41 +0900

    fix: 상수 MESSAGE의 prefix 변수를 참조하지 못하는 문제 수정

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -6,11 +6,11 @@ export const GAME_RESULT = {
 
 export const MESSAGE = {
   prefix: `[ERROR]`,
-  isNotEmptyString: `${prefix} 공백이 포함되어 있습니다. 숫자는 연달아서 입력해주세요.`,
-  isNumber: `${prefix} 유효하지 않은 숫자입니다. 서로 다른 숫자 3개를 입력해주세요.`,
-  isAnswerInteger: `${prefix} 유효하지 않은 숫자입니다. 1이상 9이하의 정수를 입력해주세요.`,
-  isAnswerNumberInRange: `${prefix} 유효하지 않은 숫자입니다. 1이상 9이하의 숫자를 입력해주세요.`,
-  isRestartNumberInRange: `${prefix} 유효하지 않은 숫자입니다. 재시작은 1, 종료는 2를 입력해주세요.`,
-  isValidLength: `${prefix} 숫자는 3개만 입력해주세요.`,
-  isDifferentNumber: `${prefix} 유효하지 않은 숫자입니다. 서로 다른 숫자 3개를 입력해주세요.`,
+  isNotEmptyString: `${this.prefix} 공백이 포함되어 있습니다. 숫자는 연달아서 입력해주세요.`,
+  isNumber: `${this.prefix} 유효하지 않은 숫자입니다. 서로 다른 숫자 3개를 입력해주세요.`,
+  isAnswerInteger: `${this.prefix} 유효하지 않은 숫자입니다. 1이상 9이하의 정수를 입력해주세요.`,
+  isAnswerNumberInRange: `${this.prefix} 유효하지 않은 숫자입니다. 1이상 9이하의 숫자를 입력해주세요.`,
+  isRestartNumberInRange: `${this.prefix} 유효하지 않은 숫자입니다. 재시작은 1, 종료는 2를 입력해주세요.`,
+  isValidLength: `${this.prefix} 숫자는 3개만 입력해주세요.`,
+  isDifferentNumber: `${this.prefix} 유효하지 않은 숫자입니다. 서로 다른 숫자 3개를 입력해주세요.`,
 };
